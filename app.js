4acb7104a21268562970f18edff0366accd1201d
chaveroricardo <chavero.ricardo@gmail.com>
2018-12-04 19:39:17 -0600

    Segundo commit

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+window.onload = function (){
+  if (localStorage.getItem('nickName') != null){
+    
+  }
+}
 
 function registrarUsuario(){
-  let userName = document.getElementById('userName').nodeValue;
-  alert('userName');
+  let userName = document.getElementById('userName').value;
+  let nickName = document.getElementById('nickName');
+  if(userName != ""){
+    /*Saving in local storage*/
+    localStorage.setItem('nickName', userName);
+    let useNickName = localStorage.getItem('nickName');
+    nickName.innerHTML = useNickName;
+  }else{
+    alert('Ingresa un nombre válido');
+  }
 }
